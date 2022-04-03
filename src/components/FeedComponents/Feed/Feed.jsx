392b0b2d23e397f71cdd48afe2a606ed7b25a63c
doxo44 <skubidu2@inbox.ru>
2022-04-03 16:00:25 +0400

    sdf

--- src/components/FeedComponents/Feed/Feed.jsx
+++ src/components/FeedComponents/Feed/Feed.jsx
@@ -1,19 +1,22 @@
-import React, {useState} from 'react'
-import SwipeProfiles from '../Swipe_Profiles/SwipeProfiles'
-import "./Feed.css"
-import SortBtn from './SortBtn'
+import React, { useState } from "react";
+import SwipeProfiles from "../Swipe_Profiles/SwipeProfiles";
+import "./Feed.css";
+
 const Feed = () => {
-    const [activeState, setActiveState] = useState("New")
+  const [newActive, setNewActive] = useState(true);
+  const [popularActive, setPopularActive] = useState(false);
+  const [favouriteActive, setFavouriteActive] = useState(false);
+
   return (
     <div className="feed">
-        <SwipeProfiles />
-        <div className="sort_btns_wrapper">
-          <SortBtn setActiveState={setActiveState} activeState={activeState} parent="New"/>
-          <SortBtn setActiveState={setActiveState} activeState={activeState} parent="Popular"/>
-          <SortBtn setActiveState={setActiveState} activeState={activeState} parent="Favourite"/>
-        </div>
+      <SwipeProfiles />
+      <div className="sort_btns_wrapper">
+        <button>New</button>
+        <button>Popular</button>
+        <button>Favourite</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
