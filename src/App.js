78fb8ee7e599a0d32a6cdbb605a8ded111a21743
doxo44 <skubidu2@inbox.ru>
2022-04-03 16:11:03 +0400

    sdfsfsdfsf

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import Posts from "./pages/Posts";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/posts" element={<Posts />} />
         </Routes>
       </BrowserRouter>
     </div>
